3fa87fe0ea66204671a407895c9920bd3576fd70
tsuyoshi.shima <acc46trera@gmail.com>
2022-02-08 07:57:52 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+   
+   
     def index
       @tasks = Task.all
     end 
@@ -50,7 +52,7 @@ class TasksController < ApplicationController
     private
     
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :status)
     end
     
 end
